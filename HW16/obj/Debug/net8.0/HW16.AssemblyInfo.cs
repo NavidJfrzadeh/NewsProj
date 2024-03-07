1affe3f5133e33aa3f5c0f53e1882001cf25b7c9
NavidJafarzadeh <navid.jfrzadeh@gmail.com>
2024-03-07 11:55:10 +0330

    add template to layout and login and Register pages

--- HW16/obj/Debug/net8.0/HW16.AssemblyInfo.cs
+++ HW16/obj/Debug/net8.0/HW16.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HW16")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2f22cb4818f76f39c622c2e16ae0baf0f1d77e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68dc6e6b5a263758a3fdc7d244bdb507239211d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("HW16")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HW16")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
